fda373e46be7c20ec9c29bdd35afc2a3f5eb5315
chris-shay <christopher.jackson.shay@gmail.com>
2017-01-20 09:28:23 -0500

    Add button styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,28 @@
 			parallaxFactor: 20
 
 	};
+	class GameView {
+	  constructor(game, context) {
+	    this.game = game;
+	    this.context = context;
+	  }
 
+	  start() {
+	    requestAnimationFrame(this.animate.bind(this));
+	  }
+
+	  animate() {
+	    if (!this.game.step) {
+	      if (this.game.check) {
+	      }
+	      this.game.changeSquares();
+	      this.game.draw(this.context);
+	    }
+	    requestAnimationFrame(this.animate.bind(this));
+	  }
+	}
+	//
+	module.exports = GameView;
 	skel.breakpoints({
 		xlarge: '(max-width: 1800px)',
 		large: '(max-width: 1280px)',
